a54ec292ba9aae31ba99f4cd93e2bc482c9d6f2e
karivarkey <geevargheseregi78@gmail.com>
2024-01-30 18:43:32 +0530

    Revamped searchbar and added quick links

--- script.js
+++ script.js
@@ -19,3 +19,39 @@ function reducelinespacing(x) {
     s.style.letterSpacing = "50px";
   }
 }
+
+function datetime()
+{
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var h = new Date();
+  var date = h.getDate()
+  var month = months[h.getMonth()]
+  var year = h.getFullYear()
+
+  var hour = to12(h.getHours())
+  var minute = h.getMinutes();
+  console.log(hour[0] + " " + minute+ " " + hour[1]);
+
+  var timeElement = document.getElementById("time");
+  timeElement.textContent = hour[0]+":"+minute+" "+hour[1]
+
+  var DateElement = document.getElementById("date");
+  DateElement.textContent = date +" " + month+" " + year
+
+  setTimeout(datetime,3000);
+
+
+}
+
+function to12(hour)
+{
+  if (hour<=12){
+    var h = [hour,"AM"]
+    return h
+  }
+  else{
+    var h = [(hour-12),"PM"]
+    return h
+    
+  }
+}
